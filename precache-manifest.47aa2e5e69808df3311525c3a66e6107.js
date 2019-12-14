a1ca78306353db02b72650c46d993ca5382ff3bb
Dao Duy Phuong <daoduyphuong0995@gmail.com>
2019-12-14 17:36:47 +0700

    Updates

---
+++ precache-manifest.47aa2e5e69808df3311525c3a66e6107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9975634b3efe4ae718526c85052e15c",
+    "url": "/pet-image/index.html"
+  },
+  {
+    "revision": "e0731a7d814a1d8250d5",
+    "url": "/pet-image/static/css/main.27074fd6.chunk.css"
+  },
+  {
+    "revision": "b71f528a634129dbcb9f",
+    "url": "/pet-image/static/js/2.b3593ebe.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/pet-image/static/js/2.b3593ebe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0731a7d814a1d8250d5",
+    "url": "/pet-image/static/js/main.963c7239.chunk.js"
+  },
+  {
+    "revision": "aef1583d9e9aab470a61",
+    "url": "/pet-image/static/js/runtime-main.94e85773.js"
+  }
+]);
