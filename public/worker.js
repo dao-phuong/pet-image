4bb2c21452f13b2124d672582cf50c8a90e6b11e
Dao Duy Phuong <daoduyphuong0995@gmail.com>
2019-12-14 18:21:42 +0700

    change icon

--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'pet-list-pwa2';
+var CACHE_NAME = 'pet-list-pwa3';
 var urlsToCache = [
   '/index.html',
 ];
@@ -16,19 +16,53 @@ self.addEventListener('install', event => {
 });
 
 // Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
+
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+  // CODELAB: Add fetch event handler here.
+  if (evt.request.url.includes('/dog.ceo')) {
+  	console.log('[ServiceWorker] Fetch (data)', evt.request.url);
+  	evt.respondWith(
+  		caches.open(DATA_CACHE_NAME).then(cache => {
+  			return fetch(evt.request)
+  				.then(response => {
+  					// If the response was good, clone it and store it in the cache
+  					if (response.status === 200) {
+  						cache.put(evt.request.url, response.clone());
+  					}
+  					return response;
+  				})
+  				.catch(err => {
+  					// Network request failed, try to get it from cache
+  					return cache.match(evt.request);
+  				})
+		}));
+  	return;
+  }
+
+  evt.respondWith(
+  	caches.open(CACHE_NAME).then(cache => {
+  		return cache.match(evt.request)
+  			.then(response => {
+  				return response || fetch(evt.request);
+  			})
+  	})
   );
 });
+
 
 // Update a service worker
 self.addEventListener('activate', event => {
